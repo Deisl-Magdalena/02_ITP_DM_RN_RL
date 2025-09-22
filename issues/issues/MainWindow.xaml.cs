38544ef20e6b2af2d3af7a27839b50b16582c266
Normàn Rainer <norman.rainer@htlwy.at>
2025-09-22 14:50:41 +0200

    c# program added

---
+++ issues/issues/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Umwandeln des Label-Inhalts in eine Ganzzahl
+            int currentValue = int.Parse(myLabel.Content.ToString());
+
+            // Den Wert um 7 verringern
+            currentValue -= 7;
+
+            // Den neuen Wert ins Label setzen
+            myLabel.Content = currentValue.ToString();
+        }
+    }
+}
+//btw here is a lot of mistakes xD
